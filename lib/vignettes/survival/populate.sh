2178773ec961882ac73268e88605951e00cd4275
Neil Shephard <n.shephard@sheffield.ac.uk>
2018-02-19 15:18:27 +0000

    Adding -e flag

--- lib/vignettes/survival/populate.sh
+++ lib/vignettes/survival/populate.sh
@@ -3,18 +3,26 @@
 ## Created          2018-02-19
 ## Author           n.shephard@sheffield.ac.uk
 ## Description      Derive follow-up specific summaries based on 'results/baseline.Rmd'
-
+##
+## Notes            Two substitutions are required, one to change the text that is used
+##                  to subset the data (the first substitution) and a second to change
+##                  the chunk names (as Rmarkdown does not allow duplicated chunk names).
 ## 6 week follow-up
-sed 's/Baseline/6 weeks/g' results/baseline.Rmd > appendix/6weeks.Rmd
+sed -e 's/Baseline/6 weeks/g' results/baseline.Rmd \
+    -e 's/baseline_/6weeks_/g' > appendix/6weeks.Rmd
 
 ## 6 month follow-up
-sed 's/Baseline/6 months/g' results/baseline.Rmd > appendix/6months.Rmd
+sed -e 's/Baseline/6 months/g' results/baseline.Rmd \
+    -e 's/baseline_/6months_/g' > appendix/6months.Rmd
 
 ## 12 month follow-up
-sed 's/Baseline/12 months/g' results/baseline.Rmd > appendix/12months.Rmd
+sed -e 's/Baseline/12 months/g' results/baseline.Rmd  \
+    -e 's/baseline_/12months_/g' > appendix/12months.Rmd
 
 ## 18 month follow-up
-sed 's/Baseline/18 months/g' results/baseline.Rmd > appendix/18months.Rmd
+sed -e 's/Baseline/18 months/g' results/baseline.Rmd  \
+    -e 's/baseline_/18months_/g' >  appendix/18months.Rmd
 
 ## 24 month follow-up
-sed 's/Baseline/24 months/g' results/baseline.Rmd > appendix/24months.Rmd
+sed -e 's/Baseline/24 months/g' results/baseline.Rmd  \
+    -e 's/baseline_/24months_/g' > appendix/24months.Rmd
